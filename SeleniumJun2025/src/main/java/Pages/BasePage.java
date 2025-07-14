1e4d1a777e1de279b0f8996926a96e863d2da631
PC <PC@DESKTOP-NV6AV6F>
2025-07-14 21:45:03 +0530

    Names added.

--- SeleniumJun2025/src/main/java/Pages/BasePage.java
+++ SeleniumJun2025/src/main/java/Pages/BasePage.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
 	
-WebDriver driver;
+WebDriver driver;           // Lavanya
+							// Naren
 	
 	public BasePage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
